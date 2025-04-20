112950442d5821fe11bf711e02a5af62707d5d56
Brooklyn <brooklyn.faletolu@student.wegc.school.nz>
2025-04-20 19:21:41 +1200

    Added buttons and labels

--- album_mngmt_system.py
+++ album_mngmt_system.py
@@ -32,4 +32,26 @@ stock = {
 
 Class AlbumGUI:        
     """Setting up GUI structure to manage stock records"""
-        
+
+    def __init__(self, root):
+        """Initialise GUI components & layout with parameters"""
+
+        self.root = root
+        self.root.title("Album Management")
+        self.root.geometry("400x600")
+
+        # Entry input & label
+        self.entry_label = Label(root, text="Input:")
+        self.entry_label.pack()
+        self.entry_box = Entry(root)
+        self.entry_box.pack()
+
+        # Output display area
+        self.output_text = Text(root, height=30, width=45)
+        self.output_text.pack()
+
+        # Buttons
+        self.btn1 = Button(root, text="Checkout Item", command=self.checkout_item)
+        self.btn1.pack()
+        self.btn2 = Button(root, text="Restock Item", command=self.restock_item)
+        self.btn2.pack()
